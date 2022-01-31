0d2b54bd51ce3d8f91b26f3bbcbb927c94c15635
Narendra Vidda <narendra@monkey.travel>
2022-01-31 19:43:09 +0530

    responsivenesss is done but for phone remainig

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,13 +18,13 @@ function Contact() {
         />
       </div>
 
-      <div className='flex-1 flex flex-col items-center justify-center' onSubmit={handleSubmit}>
+      <div className='flex-1 flex flex-col sm:space-y-1 lg:space-y-2 items-center justify-center' onSubmit={handleSubmit}>
         <h2 className='text-[30px]'>Contact</h2>
-        <span className='w-1/5 h-0.5 bg-black'></span>
-        <form className='w-[70%] h-[70%] flex flex-col items-center justify-around'>
-          <input type="text" placeholder="Email" className='w-[300px] h-[30px] text-[14px] border-2 border-slate-300 border-solid rounded-sm p-3'/>
+        <span className='w-1/5 h-0.5 bg-black my-1'></span>
+        <form className='w-[70%] h-[70%] flex flex-col my-2 items-center justify-around'>
+          <input type="text" placeholder="Email" className='w-[300px] my-2 h-[30px] text-[14px] border-2 border-slate-300 border-solid rounded-sm p-3'/>
           <textarea placeholder='Message' className='w-[300px] h-[200px] text-[14px] border-2 border-slate-300 border-solid rounded-sm p-3'></textarea>
-          <button type='submit' className='w-[150px] h-[30px] bg-pink-700 border-none font-medium cursor-pointer  text-white rounded-lg hover:bg-green-800 focus:outline-none'>Send</button>
+          <button type='submit' className='w-[150px] h-[30px] my-2 bg-pink-700 border-none font-medium cursor-pointer  text-white rounded-lg hover:bg-green-800 focus:outline-none'>Send</button>
           {message && <span className='text-green-800 font-medium text-xl'>Thanks! I 'll replay ASAP:)</span>}
         </form>
       </div>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -7,8 +7,8 @@ function Testimonials() {
   const data = TestimonialsData;
 
   return <div id="testimonials" className='bg-white w-screen h-[calc(100vh_-_64px)] snap-start flex  flex-col items-center'>
-      <h1 className='text-[50px] font-semibold'>Testimonials</h1>
-      <div className='w-full h-[80%] flex items-center justify-center'>
+      <h1 className='sm:text-[28px] md:text-[36px] lg:text-[50px] font-semibold'>Testimonials</h1>
+      <div className='sm:w-full md:w-11/12 lg:w-full h-[80%] flex items-center justify-center'>
       {data.map(testimonial => {
         return <TestimonialsCard name={testimonial.name} title={testimonial.title} desc={testimonial.desc} img={testimonial.img} icon={testimonial.icon} featured={testimonial.featured}/>
       })}
