407f0786060a64199554103ab44f915d64dcac52
Narendra Vidda <narendra@monkey.travel>
2022-02-01 00:13:28 +0530

    Updates

--- src/components/PortfolioList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function PortfolioList({ id, title, isActive, setSelected}) {
-  return <li onClick={() => setSelected(id)} className={isActive ? 'portfolio_list bg-slate-800 text-white' : 'portfolio_list hover:text-white hover:bg-slate-800'}>{title}</li>;
-}
-
-export default PortfolioList;
