11e97afd3e8e28c3642717a42c100f29166948aa
Narendra Vidda hub <73091142+nvidda-hub@users.noreply.github.com>
2022-02-19 19:06:59 +0530

    added testimonial content

--- src/components/TestimonialsData.js
+++ src/components/TestimonialsData.js
@@ -4,7 +4,7 @@ export const TestimonialsData = [
       name: "Charan Pradbhu Monkey",
       title: "Web Developer",
       img:
-        "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
         icon: "images/linkedin.png",
       desc:
         "First he plan the task and then clear them out. Most helful guy and always ready to help.",
@@ -12,24 +12,24 @@ export const TestimonialsData = [
       },
     {
       id: 2,
-      name: "Alex Kalinski",
-      title: "Co-Founder of DELKA",
+      name: "Rajat Bishnoi",
+      title: "SDE III at amazon",
       img:
-        "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
       icon: "images/youtube.png",
       desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+        "He is good coder. Keep trying to solve more and more problem. ",
       featured: true,
     },
     {
       id: 3,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
+      name: "Vaibhav Kumar",
+      title: "Software Developer at ShareChat",
       img:
-        "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://media-exp1.licdn.com/dms/image/C5603AQGlt2vNwQnypg/profile-displayphoto-shrink_400_400/0/1517552732659?e=1650499200&v=beta&t=g35astbb4NhhvFqS343ppBkhcpVxb0c1Vu1tf-tUzmA",
       icon: "images/linkedin.png",
       desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+        "He is quick learner and flexible to enviroment.",
       featured: false,
       },
   ];
